eaddf0a1720b4256c910ede328279fc86c3b3097
Jonathan Mendoza <jonathanmendoza2001.jm@gmail.com>
2023-08-08 17:00:25 -0600

    initial commit

---
+++ ejemplo2/js/triangulo.js
@@ -0,0 +1,5 @@
+var base = prompt('Introduzca la base de el triángulo:','');
+var altura = prompt('Introduzca la altura de el triángulo:','');
+var area;
+area = (base*altura)/2;
+document.write("<header><h1>El área del triángulo es: " + area + "</h1><hr /><br /></header>");
